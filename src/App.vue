52268c349ff08e6ce6f4352565fb4d710ae58da9
akshat.soni <akshat.soni@finflock.com>
2021-05-11 19:20:25 +0530

    README & Screenshots updated

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,10 @@
         <div class="input-group mb-3">
           <input type="text" class="form-control" placeholder="Task Name" v-model="taskName">
           <div class="input-group-append">
-            <button class="btn btn-success" type="submit">
+            <button v-if="update" class="btn btn-warning" type="submit">
+              <i class="fa fa-pencil" aria-hidden="true"></i>
+            </button>
+            <button v-else class="btn btn-success" type="submit">
               <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
           </div>
@@ -21,7 +24,7 @@
       <ul class="list-group">
         <li v-for="task_name in tasks" :key="task_name" class="list-group-item list-group-item-info">
           <div class="row">
-            <div class="col">{{ task_name }}</div>
+            <div class="col" @click="loadData(task_name)">{{ task_name }}</div>
             <div class="col text-right">
               <i class="fa fa-trash" aria-hidden="true" @click="delTask(task_name)"></i>
             </div>
@@ -31,7 +34,7 @@
     </div>
   </div>
 </template>
-
+// updateIndex=taskName; taskName=task_name; update=true
 <script>
 export default {
   name: 'app',
@@ -41,24 +44,49 @@ export default {
       msg: 'Welcome to Your First ToDo List App with Vue.js',
       taskName: "",
       tasks: ['Download VueJS', 'Install Node Modules', 'Run NodeJS Server'],
-      error: false
+      error: false,
+      update: false,
+      updateIndex: null
     }
   },
   methods: {
+    loadData: function(item){
+      this.taskName=item
+      console.log(this.taskName)
+      this.updateIndex = this.taskName
+      this.update=true
+    },
     addTask: function(){
-      if(this.taskName != ''){
-        this.tasks.push(this.taskName)
+      if(this.update == true){
+
+        this.update = false
+
+        var ind = this.tasks.indexOf(this.updateIndex)
+        console.log(this.updateIndex)
+        this.tasks[ind] = this.taskName
+
       }
       else{
-        this.error = true
+
+        if(this.taskName != ''){
+          this.tasks.push(this.taskName)
+        }
+        else{
+          this.error = true
+        }
+
       }
+
       this.taskName = ''
+      console.log(this.tasks)
     },
     delTask: function(taskname){
       this.tasks = this.tasks.filter((task)=>{
         return task != taskname
       })
-    }
+      this.update = false
+      this.taskName = ''
+    },
   }
 }
 </script>
